7281fa4f9efe68de1548fa92b8232e770b85daa3
stetsyuk <52718548+stetsyuk@users.noreply.github.com>
2019-09-27 13:13:49 +0300

    Fix bad.py - I

--- bad.py
+++ bad.py
@@ -1,9 +1,12 @@
 import sys
-#No empty line after imports, no space after comment
-x=1
-y=4/3
-x+=y
-def f(z, a = 16):
-  return z+a
- q=f(14)
- print(f(45,q))
+
+
+# Fix this file =)
+x = 1
+y = 4 / 3
+x += y
+
+def f(z, a=16):
+  return z + a
+ q = f(14)
+ print(f(45, q))
